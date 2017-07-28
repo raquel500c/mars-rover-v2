ae17f3ddd53f2f0b1d66dcdbcaae21215926ba31
raquel500c <raquel500c@gmail.com>
2017-07-28 20:52:31 +0200

    "v2"

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,8 +3,9 @@
 var rover = {
   direction: "N",
   x: 0,
-  y: 0
-}
+  y: 0,
+  travelLog: ["[0, 0]"],
+};
 
 // ======================
 function turnLeft(rover) {
@@ -25,7 +26,6 @@ function turnLeft(rover) {
 
   }
   console.log("turnLeft was called!");
-
 }
 
 function turnRight(rover) {
@@ -49,32 +49,99 @@ function turnRight(rover) {
 }
 
 function moveForward(rover) {
+  var displaced = false;
+  console.log("moveForward was called!");
   switch (rover.direction) {
     case 'N':
-      rover.y += 1;
+      if (rover.y === 9) {
+        console.log("### Rover not displaced,  NORTH BORDER ###");
+      } else {
+        rover.y += 1;
+        displaced = true;
+      }
       break;
     case 'S':
-      rover.y -= 1;
+      if (rover.y === 0) {
+        console.log("### Rover not displaced, SOUTH BORDER ###");
+      } else {
+        rover.y -= 1;
+        displaced = true;
+      }
       break;
     case 'E':
-      rover.x += 1;
+      if (rover.x === 9) {
+        console.log("### Rover not displaced, EAST BORDER ###");
+      } else {
+        rover.x += 1;
+        displaced = true;
+      }
       break;
     case 'W':
-      rover.x -= 1;
+      if (rover.x === 0) {
+        console.log("### Rover not displaced, WEST BORDER ###");
+      } else {
+        rover.x -= 1;
+        displaced = true;
+      }
       break;
-    default:
-
   }
-  console.log("moveForward was called");
-  console.log("Current Direction: " + rover.direction);
-  console.log("Current Position: " + rover.x + "," + rover.y);
+  if (displaced) {
+    var newPosition = " [" + rover.x + ", " + rover.y + "]";
+    rover.travelLog.push(newPosition);
+  }
+}
+
+function moveBack(rover) {
+  var displaced = false;
+  console.log("moveForward was called!");
+  switch (rover.direction) {
+    case 'S':
+      if (rover.y === 9) {
+        console.log("### Rover not displaced,  NORTH BORDER ###");
+      } else {
+        rover.y += 1;
+        displaced = true;
+      }
+      break;
+    case 'N':
+      if (rover.y === 0) {
+        console.log("### Rover not displaced, SOUTH BORDER ###");
+      } else {
+        rover.y -= 1;
+        displaced = true;
+      }
+      break;
+    case 'W':
+      if (rover.x === 9) {
+        console.log("### Rover not displaced, EAST BORDER ###");
+      } else {
+        rover.x += 1;
+        displaced = true;
+      }
+      break;
+    case 'E':
+      if (rover.x === 0) {
+        console.log("### Rover not displaced, WEST BORDER ###");
+      } else {
+        rover.x -= 1;
+        displaced = true;
+      }
+      break;
+  }
+  if (displaced) {
+    var newPosition = " [" + rover.x + ", " + rover.y + "]";
+    rover.travelLog.push(newPosition);
+  }
 }
 
 function moveRover(str) {
-  for (i = 0; i < cadena.length; i++) {
+  for (i = 0; i < str.length; i++) {
     switch (str[i]) {
       case 'f':
         moveForward(rover);
+        break;
+      case 'b':
+        moveBack(rover);
         break;
       case 'l':
         turnLeft(rover);
@@ -83,9 +150,10 @@ function moveRover(str) {
         turnRight(rover);
         break;
       default:
-        console.log(str[i] + ' no es una orden válida');
+        console.log('"' + str[i] + '"' + ' no es una orden válida');
     }
   }
-  console.log("Current Direction: " + rover.direction);
-  console.log("Current Position: " + rover.x + "," + rover.y);
+  console.log("Rover Travel => " + rover.travelLog);
+  console.log("Current Rover Direction: " + rover.direction);
+  console.log("Current Rover Position: " + "[" + rover.x + ", " + rover.y + "]");
 }
